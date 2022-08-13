1d0dd41a89ff3df12812fecde5dc59a20f584d3f
HiagoCS <hiago8cs@gmail.com>
2022-08-13 16:03:18 -0300

    criado a migration de tb_privilegio

---
+++ app/Database/Migrations/2022-08-13-190051_TbPrivilegio.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TbPrivilegio extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' =>[
+                'type' =>'INT',
+                'auto_increment' =>true
+            ],
+            'nome' =>[
+                'type' =>'VARCHAR',
+                'constraint' => 199
+            ],
+            'senha' =>[
+                'type' =>'VARCHAR',
+                'constraint' => 199,
+                'null' => TRUE
+            ]
+            ]);
+        $this->forge->addPrimaryKey('id', TRUE);
+        $this->forge->createTable('tb_privilegio', TRUE);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_privilegio', TRUE);
+    }
+}
