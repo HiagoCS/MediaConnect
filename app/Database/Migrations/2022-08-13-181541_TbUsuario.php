8e8c6a5a157edd210e3881a4050e9b425f6620a0
HiagoCS <hiago8cs@gmail.com>
2022-08-13 15:52:13 -0300

    criado a migration de tb_usuario

---
+++ app/Database/Migrations/2022-08-13-181541_TbUsuario.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TbUsuario extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' =>[
+                'type' =>'INT',
+                'auto_increment' =>true
+            ],
+            'nome' =>[
+                'type' =>'VARCHAR',
+                'constraint' => 199
+            ],
+            'email' =>[
+                'type' =>'LONGTEXT'
+            ],
+            'senha' =>[
+                'type' =>'VARCHAR',
+                'constraint' => 199
+            ],
+            'cpf' =>[
+                'type' =>'VARCHAR',
+                'constraint' => 15
+            ],
+            'telefone' =>[
+                'type' =>'VARCHAR',
+                'constraint' => 20
+            ],
+            'cep' =>[
+                'type' =>'VARCHAR',
+                'constraint' => 10
+            ],
+            'status' =>[
+                'type' =>'VARCHAR',
+                'constraint' => 199
+            ]
+
+            ]);
+            $this->forge->addPrimaryKey('id', TRUE);
+            $this->forge->createTable('tb_usuario', TRUE);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tbUsuario', TRUE);
+    }
+}
