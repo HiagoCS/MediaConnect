59da49f0f7980928f5be6ddd330c1d78a5618173
HiagoCS <hiago8cs@gmail.com>
2022-08-13 16:20:24 -0300

    criado a migration de tb_anuncio

---
+++ app/Database/Migrations/2022-08-13-191238_TbAnuncio.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TbAnuncio extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' =>[
+                'type' =>'INT',
+                'auto_increment' =>true
+            ],
+            'id_vendedor' =>[
+                'type' =>'INT'
+            ],
+            'id_servico' =>[
+                'type' =>'INT'
+            ],
+            'nome' =>[
+                'type' =>'VARCHAR',
+                'constraint' => 199
+            ],
+            'valor' =>[
+                'type' =>'FLOAT'
+            ],
+            'qt_vendas' =>[
+                'type' =>'INT'
+            ],
+            'md_avaliacao' =>[
+                'type' =>'INT',
+                'constraint' => 10
+            ],
+            'descricao' =>[
+                'type' => 'LONGTEXT'
+            ]
+        ]);
+        $this->forge->addForeignKey('id_vendedor', 'tb_usuario', 'id');
+        $this->forge->addForeignKey('id_servico', 'tb_servico', 'id');
+        $this->forge->addPrimaryKey('id', TRUE);
+        $this->forge->createTable('tb_anuncio', TRUE);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_anuncio', TRUE);
+    }
+}
