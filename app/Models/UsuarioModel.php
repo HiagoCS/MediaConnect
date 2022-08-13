e9c5b8ef8ef6801746510d3ad8e08b65787868d3
HiagoCS <hiago8cs@gmail.com>
2022-08-13 19:17:09 -0300

    criado model UsuarioModel

---
+++ app/Models/UsuarioModel.php
@@ -0,0 +1,10 @@
+<?php namespace App\Models;
+    use CodeIgniter\Model;
+
+    class UsuarioModel extends Model{
+        protected $table = 'tb_usuario';
+        protected $primaryKey = 'id';
+        protected $allowedFields = ['nome', 'email', 'senha', 'cpf', 'telefone', 'cep', 'status'];
+        protected $returnType = 'object';
+    }
+?>
