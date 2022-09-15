506110b57d5449478ccdd7ef307513ce28914b0c
HiagoCS <hiago8cs@gmail.com>
2022-09-15 03:46:00 -0300

    Finalizado inserção com atualização de média

--- app/Models/AvaliacaoModel.php
+++ app/Models/AvaliacaoModel.php
@@ -1,27 +1,40 @@
 <?php namespace App\Models;
     use CodeIgniter\Model;
 
-    class AvalicaoModel extends Model{
+    class AvaliacaoModel extends Model{
         protected $table = 'tb_avaliacao';
         protected $primaryKey = 'id';
-        protected $allowedFields = ['id_cliente', 'id_anuncio', 'num_avalicao', 'msg_avaliacao'];
+        protected $allowedFields = ['id_cliente', 'id_anuncio', 'num_avaliacao', 'msg_avaliacao'];
         protected $returnType = 'object';
 
-        public function postInserir($avalicao_data){
+        public function postInserir($avaliacao_data){
             $usuarioModel = new \App\Models\UsuarioModel();
             $anuncioModel = new \App\Models\AnuncioModel();
             if($usuarioModel->find($avalicao_data['id_cliente'])){
-                if($anuncioModel->find($avalicao_data['id_anuncio'])){
-                    if($this->insert($avalicao_data)){
+                if($anuncioModel->find($avaliacao_data['id_anuncio'])){
+                    if($this->insert($avaliacao_data)){
                         //Sucesso no registro!!
-                        echo 'Sucesso no registro!!';
+                        if($anuncioModel->updateAvaliacao($avaliacao_data['id_anuncio'])){
+                            //Média de avaliação do anuncio atualizada
+                            echo "Sucesso no registro!!";
+                            return;
+                        }
+                        else{
+                            //Erro na atualização da média
+                            echo "Erro na atualização da média!! Verifique se os itens abaixo estão de acordo!!<br>";
+                            for($i = 0; $i < count($avaliacao_data); $i++){
+                                echo "<br>".strtoupper(key($avaliacao_data))." = ".current($avaliacao_data)."<br>";
+                                next($avaliacao_data);
+                            }
+                            return;
+                        }
                     }
                     else{
                         //Erro no registro!!
                         echo "Erro no registro!! Verifique se os itens abaixo estão de acordo!!<br>";
-                        for($i = 0; $i < count($avalicao_data); $i++){
-                            echo "<br>".strtoupper(key($avalicao_data))." = ".current($avalicao_data)."<br>";
-                            next($avalicao_data);
+                        for($i = 0; $i < count($avaliacao_data); $i++){
+                            echo "<br>".strtoupper(key($avaliacao_data))." = ".current($avaliacao_data)."<br>";
+                            next($avaliacao_data);
                         }
                         return;
                     }
@@ -29,9 +42,9 @@
                 else{
                     //Erro na inserção de anuncio!!
                     echo "Erro na inserção de anuncio!! Verifique se os itens abaixo estão de acordo!!<br>";
-                    for($i = 0; $i < count($avalicao_data); $i++){
-                        echo "<br>".strtoupper(key($avalicao_data))." = ".current($avalicao_data)."<br>";
-                        next($avalicao_data);
+                    for($i = 0; $i < count($avaliacao_data); $i++){
+                        echo "<br>".strtoupper(key($avaliacao_data))." = ".current($avaliacao_data)."<br>";
+                        next($avaliacao_data);
                     }
                     return;
                 }
@@ -39,9 +52,9 @@
             else{
                 //Erro na inserção de cliente!!
                 echo "Erro na inserção de cliente!! Verifique se os itens abaixo estão de acordo!!<br>";
-                for($i = 0; $i < count($avalicao_data); $i++){
-                    echo "<br>".strtoupper(key($avalicao_data))." = ".current($avalicao_data)."<br>";
-                    next($avalicao_data);
+                for($i = 0; $i < count($avaliacao_data); $i++){
+                    echo "<br>".strtoupper(key($avaliacao_data))." = ".current($avaliacao_data)."<br>";
+                    next($avaliacao_data);
                 }
                 return;
             }
